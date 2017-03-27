1aa026782bd1110a13c8ee07cfddbfde2c3898c0
mkwebdevelopment230 <mkwebdevelopment.230@gmail.com>
2017-03-27 15:17:36 +0530

    Add files via upload

---
+++ default2.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @version    CVS: 1.0.0
+ * @package    Com_Datadirectory
+ * @author     raj <raj.k230@gmail.com>
+ * @copyright  2017 raj
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+
+?>
+
+<div class="item_fields">
+
+	<table class="table">
+		
+
+	</table>
+
+</div>
+
